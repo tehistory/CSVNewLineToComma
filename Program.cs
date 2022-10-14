ac4d398f37e8adda68ac09c3357b67b16754e54d
Frank Egelhoff <ftegelhoff@gmail.com>
2022-10-14 15:56:59 -0500

    Project Upload
    its basically done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input file");
+string file = Console.ReadLine();
+string output = "";
+StreamReader reader = new StreamReader(File.OpenRead(file));
+
+while (!reader.EndOfStream)
+{
+    output += reader.ReadLine() + ',';
+}
+
+reader.Close();
+
+Console.WriteLine("Output File");
+string outFile = Console.ReadLine();
+
+StreamWriter writer = new StreamWriter(File.Create(outFile));
+writer.WriteLine(output);
+writer.Close();
